6a2782b8b3bf331f63fda743d7ce0b49564aad94
Alishazakir <alishazakir131@gmail.com>
2026-07-13 10:58:32 +0530

    python learning : started with first file

---
+++ firstprogram.py
@@ -0,0 +1,61 @@
+print("hello world!")
+print("my name is alisha" , "my age is 19")
+    #variables
+name = "alisha"
+age = 19
+age2 = age
+price = 100
+old = False
+price1 = 5.5
+a = None
+
+print(name)
+print(age)
+print(price)
+print("my name is :", name)
+print(age2)
+     #data types integers, string, float, boolean, none.
+print(type(name))
+print(type(age))
+print(type(price))
+print(type(old))
+print(type(price1))
+print(type(a))
+       #print sum
+a = 2
+b = 3
+sum = a  + b
+print(sum)
+#this a comment
+"""
+multi-line
+comment
+"""
+# types of operators 
+print(a == b)
+print(a!= b)
+print(a <= b)
+print(a > b)
+num = 10
+num += 10  #num = num + 10, and same is for -&*%
+print("num :", num)
+print(not True)
+print(not False)
+val1 = True
+val2 = True
+print("and operator:", val1 and val2)
+    #type conversion
+a = 2
+b = 4.2
+print(2 + 4.2) 
+print(type(a + b))
+a = int("2")
+b = 4.25
+print(a + b)
+print(type(a + b))
+   #input in python
+name = input("enter your name :")
+age = input("enter your age :")
+
+print("welcome", name)
+print("age =", age)
